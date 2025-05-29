e5b7ff5c0f86ff36f7509c4d28b6f122f315ead6
Rys17 <ryslann95@mail.ru>
2025-05-30 02:05:37 +0300

    Создал маршрутизацию и изменил название страниц

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,12 +1,52 @@
-import Main from '../pages/main';
+import Main from '../pages/main-page';
+import Login from '../pages/login-page';
+import Favorites from '../pages/favorites-page';
+import Offer from '../pages/offer-page';
+import NotFoundPage from '../pages/not-found-page';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Router
+} from 'react-router-dom';
+import { AppRoute } from '../const';
 
 type AppProps = {
   offerCounter: number;
 }
 
-function App({offerCounter}:AppProps) {
+function App({ offerCounter }: AppProps) {
   return (
-    <Main offerCounter={offerCounter}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main offerCounter={offerCounter} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<Favorites />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<Offer />}
+        />
+
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+
+      </Routes>
+
+
+    </BrowserRouter>
+
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -14,4 +14,4 @@ root.render(
   </React.StrictMode>
 );
 
-export default OFFER_COUNTER;
+
